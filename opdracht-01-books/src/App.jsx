47446e94239a880649a07939ffabdd8d31ca96d7
YanikRijgersberg <9024941@student.zadkine.nl>
2025-10-08 10:01:13 +0100

    dwad

--- opdracht-01-books/src/App.jsx
+++ opdracht-01-books/src/App.jsx
@@ -1,10 +1,16 @@
 import './App.css'
+import Book from './components/Book'
+
 
 function App() {
 
   return (
     <>
       <h1>Opdracht 1 - Books</h1>
+
+      <Book img="./public/images/book-1.png" title="harry snotter" beschrijving="leuk booek voor een tover kind"/>
+      <Book img="./public/images/book-2.png" title="fantasia 6" beschrijving="leuk dik boek over een interesante wereld "/>
+      <Book img="./public/images/book-3.png" title="the hunger games" beschrijving="honger spelen"/>
     </>
   )
 }
