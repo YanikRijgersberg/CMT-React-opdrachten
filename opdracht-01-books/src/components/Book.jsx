aaec328b832ffce0b30e1dc81e2d33468b9e97da
YanikRijgersberg <9024941@student.zadkine.nl>
2025-12-16 10:58:36 +0000

    b

--- opdracht-01-books/src/components/Book.jsx
+++ opdracht-01-books/src/components/Book.jsx
@@ -1,9 +1,17 @@
 import { useState } from "react";
-const Book = ({ img, title, author, verhoogTeller,category }) => {
+
+const Book = ({ img, title, author, category }) => {
   const [aantalKeerGelezen, setAantalKeerGelezen] = useState(0);
-  function verhoogTeller() {
+
+  const [liked, setLiked] = useState(false);
+
+  const verhoogLeesTeller = () => {
     setAantalKeerGelezen(aantalKeerGelezen + 1);
-  }
+  };
+
+  const toggleLike = () => {
+    setLiked(!liked);
+  };
 
   return (
     <article className="book">
@@ -11,9 +19,25 @@ const Book = ({ img, title, author, verhoogTeller,category }) => {
       <h2>{title}</h2>
       <h3>{author}</h3>
       <p>Categorie: {category}</p>
-      <button onClick={verhoogTeller}>Aantal keer gelezen: {aantalKeerGelezen}</button>
+
+      <button onClick={verhoogLeesTeller}>
+        Aantal keer gelezen: {aantalKeerGelezen}
+      </button>
+
+      {/* Favoriet sectie */}
+      <div className="favorite-section">
+        <button onClick={toggleLike}>
+          {liked ? "❤️" : "🤍"}
+        </button>
+
+        {liked && (
+          <p className="favorited-text">
+            Toegevoegd aan favorieten
+          </p>
+        )}
+      </div>
     </article>
-    
   );
 };
+
 export default Book;
